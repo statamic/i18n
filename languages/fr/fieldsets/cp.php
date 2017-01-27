d6a919bc4f4d3df0f0e15b1fc0101769762ec68b
Emmanuel Beauchamps <e.bops@hotmail.fr>
2017-01-27 21:30:42 +0100

    2.5.1 French (missing file)
    oops, forgot to translate 1 file in the previous commit…

--- languages/fr/fieldsets/cp.php
+++ languages/fr/fieldsets/cp.php
@@ -8,6 +8,9 @@ return [
     'date_format' => 'Format de date',
     'date_format_instruct' => 'Définissez le format d\'affichage des colonnes comportant les dates de vos articles.',
     
+    'pagination_size' => 'Taille de pagination',
+    'pagination_size_instruct' => 'Le nombre d\'éléments à afficher sur chaque page paginée.',
+    
     'widgets' => 'Widgets du tableau de bord',
 
 ];
