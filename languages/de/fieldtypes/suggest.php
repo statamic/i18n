3e20286effa48cea0ee4b78bfa7ef47fc9dcdbfb
Andreas Schantl <andreas.schantl@gmail.com>
2018-04-18 03:29:35 +0200

    Update suggest.php

--- languages/de/fieldtypes/suggest.php
+++ languages/de/fieldtypes/suggest.php
@@ -5,8 +5,8 @@ return [
     'options' => 'Optionen',
     'options_instruct' => 'Erfasse für jede Option links einen Wert und rechts eine Beschreibung.',
     'max_items' => 'Max. Einträge',
-    'max_items_instruct' => 'Die maximale Anzahl Einträge die ausgewählt werden kann.',
+    'max_items_instruct' => 'Die maximale Anzahl von Einträgen, die ausgewählt werden kann.',
     'create' => 'Neue Optionen automatisch erstellen?',
-    'create_instruct' => 'Wenn aktiviert, können Werte selbstständig erstellt werden anstatt nur aus der Liste auszuwählen.',
+    'create_instruct' => 'Wenn aktiviert, können Werte selbstständig erstellt werden, anstatt nur aus der Liste auszuwählen.',
 
 ];
