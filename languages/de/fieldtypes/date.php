86d055b6831f488c462cbdf6ce68fd6ec1d4b660
Andreas Schantl <andreas.schantl@gmail.com>
2018-05-23 23:14:26 +0200

    Update date.php
    Update the file according to its English counterpart

--- languages/de/fieldtypes/date.php
+++ languages/de/fieldtypes/date.php
@@ -2,13 +2,19 @@
 
 return [
 
-    'allow_blank' => 'Erlaube leere Angabe?',
-    'allow_blank_instruct' => 'Wenn aktiviert, kann das Datum geleert werden.',
+    'allow_blank' => 'Erlaube leere Angabe?', // 'Allow blank values?'
+    'allow_blank_instruct' => 'Wenn aktiviert, kann das Datum geleert werden.' // 'If enabled, you will be able to clear the date.',
 
-    'allow_time' => 'Erlaube Uhrzeiten?',
-    'allow_time_instruct' => 'Wenn aktiviert, kann eine Uhrzeit gewählt werden.',
+    'allow_time' => 'Erlaube Uhrzeiten?', // 'Allow times?'
+    'allow_time_instruct' => 'Wenn aktiviert, kann eine Uhrzeit gewählt werden.', // 'If enabled, you will be able to select a time.'
+    
+    'require_time' => 'Zeit benötigt?', // 'Require time?',
+    'require_time_instruct' => 'Wenn aktiviert, dann muss eine Zeit gewählt werden.', // 'If enabled, you will be forced to select a time.',
 
-    'earliest_date' => 'Frühstes Datum',
-    'earliest_date_instruct' => 'Wähle das frühste auswählbare Datum.',
+    'earliest_date' => 'Frühstes Datum', // 'Earliest Date'
+    'earliest_date_instruct' => 'Wähle das frühste auswählbare Datum.' // 'Set the earliest selectable date.',
 
+    'format' => 'Datumsformat', // 'Date Format'
+    'format_instruct' => 'Wie das Datum angezeigt werden sollte. Alle [PHP Datumsformate](http://php.net/manual/en/function.date.php) können benutzt werden.', // 'How the date should be saved. Any [PHP date formatting variables](http://php.net/manual/en/function.date.php) may be used.'
+    
 ];
