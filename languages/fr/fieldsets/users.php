3b80a43a205c3ff7963d4196c7d1b3de2f609ef6
Emmanuel Beauchamps <e.bops@hotmail.fr>
2018-07-18 22:15:03 +0200

    2.10.0 ENGLISH and FRENCH

--- languages/fr/fieldsets/users.php
+++ languages/fr/fieldsets/users.php
@@ -7,7 +7,7 @@ return [
                               Si positionné sur "Email Address" (Adresse mail), les utilisateurs ne pourront se connecter qu’avec leur email et les noms de fichiers seront les adresses emails.',
                     
     'new_user_roles' => 'Nouveaux rôles utilisateurs',
-    'new_user_roles_instruct' => 'Ces rôles seront automatiquement affectés à tous les utilisateurs qui s’inscriront via le frontal de votre site.',
+    'new_user_roles_instruct' => 'Ces rôles seront automatiquement affectés à tous les utilisateurs inscrits via le frontal de votre site ou à ceux créés par des utilisateurs n’ayant pas les droits pour modifier les permissions.',
     
     'enable_gravatar' => 'Activer Gravatar',
     'enable_gravatar_instruct' => 'Utilisez <a href="http://gravatar.com" target="_blank">Gravatar.com</a> comme source d’Avatars utilisateurs automatiques.',
