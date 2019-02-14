2bf3c3e34f156c85293c5ab12411032922d9961e
Rob de Kort <rob@studio1902.nl>
2019-02-14 23:33:51 +0100

    Update and add others

--- languages/nl/markdown.php
+++ languages/nl/markdown.php
@@ -3,7 +3,7 @@
 return [
 
     'cheatsheet' => '
-<p>Markdown is een text-naar-HTML syntax voor web schrijvers. Markdown staat je toe om te schrijven in een gemakkelijk te lezen, gemakkelijk te schrijven textformaat dat geconverteerd wordt naar HTML.</p>
+<p>Markdown is een text-naar-HTML syntax voor web schrijvers. Markdown staat uw toe om te schrijven in een gemakkelijk te lezen, gemakkelijk te schrijven formaat dat omgezet wordt naar HTML.</p>
 
 <h3>Headers</h3>
 <pre class="language-markdown"><code># Dit is een h1
@@ -12,25 +12,25 @@ return [
 </code></pre>
 
 <h3>Vet &amp; Italic</h3>
-<pre class="language-markdown"><code>Je kan zaken *schuin*, **bold**, of _**beide**_ maken.</code></pre>
+<pre class="language-markdown"><code>U kunt zaken *schuin*, **bold**, of _**beide**_ maken.</code></pre>
 
 <h3>Links</h3>
 <pre class="language-markdown"><code>Dit is een [voorbeeld link](http://example.com).</code></pre>
 
 <h3>Code</h3>
 <p>
-Omarm je code met 3 backticks (<code>```</code>) op de lijn ervoor en erna.
+Omsluit uw code met 3 backticks (<code>```</code>) op de lijn ervoor en erna.
 </p>
 
 <pre class="language-markdown"><code>```
 dit: is een beetje yaml
 ```</code></pre>
 
-<p>Je kan ook code in een lijn zetten door de inhoud te omarmen met <code>`</code> enkele backticks.
+<p>U kunt ook code op dezelfde regel plaatsen door de inhoud te omsluiten met <code>`</code> enkele backticks.
 
 <h3>Quotes</h3>
 
-<p>Maak een blockquote aan door je tekst te starten met <code>> </code>.</p>
+<p>Maak een blockquote aan door uw tekst te starten met <code>> </code>.</p>
 
 <pre class="language-markdown"><code>> Dit is een blockquote.</code></pre>
 
@@ -49,7 +49,7 @@ dit: is een beetje yaml
 
 <h3>Tabellen</h3>
 
-<pre class="language-markdown"><code>First Header  | Second Header
+<pre class="language-markdown"><code>Eerste header  | Tweede header
 ------------- | -------------
 Content Cell  | Content Cell
 Content Cell  | Content Cell</code></pre>'
