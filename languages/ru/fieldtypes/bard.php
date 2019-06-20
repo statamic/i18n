5a31e556a8ea90d648722dddccb78cd3821194ed
Vladimir Che <vchernomyrdin@treto.ru>
2019-06-20 22:59:15 +0600

    upd, finish translation to 2.11.12, with removed outdated variables

--- languages/ru/fieldtypes/bard.php
+++ languages/ru/fieldtypes/bard.php
@@ -17,39 +17,27 @@ return [
     'restrict_assets' => 'Ограничить папку',
     'restrict_assets_instruct' => 'Запретить навигацию к другим папкам или контейнерам активов.',
         
-    'style' => 'Typography Style',
-    'style_instruct' => 'Choose your typography style.',
+    'style' => 'Стиль оформления',
+    'style_instruct' => 'Выберите свой стиль оформления.',
 
-//    'autoLink' => 'Автоматическая ссылка',
-//    'autoLink_instruct' => 'Автоматически связывать любые URL-адреса в вашем тексте.',
-        
     'spellcheck' => 'Проверка орфографии',
     'spellcheck_instruct' => 'Включить автоматическую проверку орфографии редактируемого контента.',
         
-    'target_blank' => 'Target Blank ',
+    'target_blank' => 'Новое окно',
     'target_blank_instruct' => 'Автоматически добавлять `target="_blank"` на ссылки.',
 
-    'link_noopener' => 'Set noopener',
-    'link_noopener_instruct' => 'Set `rel="noopener"` on all created links.',
+    'link_noopener' => 'Выбрать /noopener/',
+    'link_noopener_instruct' => 'Выставить `rel="noopener"` на все сылки.',
 
-    'link_noreferrer' => 'Set noreferer',
-    'link_noreferrer_instruct' => 'Set `rel="noreferrer"` on all created links.',
+    'link_noreferrer' => 'Выбрать /noreferer/',
+    'link_noreferrer_instruct' => 'Выставить `rel="noreferrer"` на все ссылки.',
 
     'allow_source' => 'Разрешить источник',
     'allow_source_instruct' => 'Разрешить просмотр и редактирование исходного кода HTML вашего текстового контента',
         
-//    'link_validation' => 'Проверка ссылок',
-//    'link_validation_instruct' => 'Проверить общие URL-протоколы по ссылкам.',
-        
-//    'force_plain_text' => 'Принудительное вложение как обычный текст',
-//    'force_plain_text_instruct' => 'Заставляет вставлять текст в виде обычного текста, удаляя любое форматирование.',
-        
-//    'clean_pasted_html' => 'Очистить вложенный HTML на вставке',
-//    'clean_pasted_html_instruct' => 'Очищает вставляемый контент из разных источников, таких как Документы Google и т. д.'
+    'semantic_elements' => 'Использовать семантические теги',
+    'semantic_elements_instruct' => 'Заменить `<b>`, `<i>`(не семантические, не релевантные теги) на `<strong>` , `<em>` теги соответственно.',
 
-    'semantic_elements' => 'Use Semantic Elements',
-    'semantic_elements_instruct' => 'Replace unsemantic `<b>` and `<i>` tags with `<strong>` and `<em>` tags.',
-
-    'allow_internal_links' => 'Allow Internal Links',
-    'allow_internal_links_instruct' => 'Allow pages and entries to be suggested as you type in the link toolbar.',
+    'allow_internal_links' => 'Разрешить внутренние ссылки',
+    'allow_internal_links_instruct' => 'Разрешить предлагать ссылки на страницы и записи как они идут на панели ссылок.',
 ];
--- languages/ru/fieldtypes/date.php
+++ languages/ru/fieldtypes/date.php
@@ -8,16 +8,16 @@ return [
     'allow_time' => 'Разрешить время?',
     'allow_time_instruct' => 'Если включено, вы сможете выбрать время.',
         
-    'require_time' => 'Require time?',
-    'require_time_instruct' => 'If enabled, you will be forced to select a time.',
+    'require_time' => 'Требуется время?',
+    'require_time_instruct' => 'Если включено, вам обязательно понадобится выбрать время.',
 
     'earliest_date' => 'Самая ранняя дата',
     'earliest_date_instruct' => 'Установите самую раннюю выбираемую дату.',
         
     'format' => 'Формат даты',
-    'format_instruct' => 'Как сохранить дату. Любые [переменные форматирования даты PHP] (http://php.net/manual/ru/function.date.php) могут использоваться. ',
+    'format_instruct' => 'Как сохранить дату. Любые [переменные даты PHP] (http://php.net/manual/ru/function.date.php) могут быть использованы. ',
 
-    'input_format' => 'Input Format',
-    'input_format_instruct' => 'How the date should be displayed in the field. Any <a href="https://momentjs.com/docs/#/displaying/format/" target="_blank">Moment.js date formatting variables</a> may be used.'
+    'input_format' => 'Формат ввода',
+    'input_format_instruct' => 'Как дата будет отображаться в поле. любые [Moment.js] (https://momentjs.com/docs/#/displaying/format/) переменные, для форматирования даты, могут быть использованы.'
 
 ];
